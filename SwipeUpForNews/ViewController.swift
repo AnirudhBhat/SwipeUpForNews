aee187cd77ef1031cb41a7dd3cec352eaebd6149
Anirudh Uppunda <abhat38@gmail.com>
2021-06-24 12:17:48 +0530

    Initial Commit

---
+++ SwipeUpForNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeUpForNews
+//
+//  Created by Deepthi on 24/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
